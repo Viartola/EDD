9612378ef4d51dd7488ef798507a0c3d957dafda
Jaime LV <Jaime LV>
2016-02-21 16:43:22 +0100

    Primer commit del primer primer metodo
    Signed-off-by: Jaime LV <Jaime LV>

--- FP/src/main/java/fp/Calculator.java
+++ FP/src/main/java/fp/Calculator.java
@@ -1,8 +1,5 @@
 package fp;
-
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.List;
+import java.util.*;
 
 public class Calculator {
 
@@ -11,59 +8,71 @@ public class Calculator {
      * este metodo devuelve el Class del object que le pasamos
      */
 	public static Class classTypeOf(Object x) {
-		throw  new NotImplementedException();
+		return x.getClass();
 	}
 
 
 	/*
-     * devuelve una lista con los n números de la serie de fibonacci.
+     * devuelve una lista con los n n?meros de la serie de fibonacci.
      */
 	public static List<Integer> fibonacci(int n) {
-		throw  new NotImplementedException();
+		
 	}
 
 	/*
-	 * Escribir todos los números del number al 0 de step en step.
+	 * Escribir todos los n�meros del number al 0 de step en step.
 	 */
 	public static int[] stepThisNumber(int number, int step) {
-		throw  new NotImplementedException();
-		}
+		
+		
+	}
 
 	/*
-	 * Módulo al que se le pasa un número entero del 0 al 20 y devuelve los
+	 * M�dulo al que se le pasa un n�mero entero del 0 al 20 y devuelve los
 	 * divisores que tiene.
 	 */
 	public static int[] divisors(int n) {
-		throw  new NotImplementedException();
+		
+		
 	}
 
 	/*
-	 * Toma como parámetros una cadena de caracteres y devuelve cierto si la cadena resulta ser un palíndromo
+	 * Toma como par�metros una cadena de caracteres y devuelve cierto si la cadena resulta ser un pal�ndromo
 	 */
 	public static boolean checkIsPalindrome(String cadena) {
-		throw  new NotImplementedException();
+		
+		
 	}
 
 	/*
-	 * Pedir un número de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56
+	 * Pedir un n�mero de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56
 	 * mostrar: cincuenta y seis
 	 */
-	public static String speakToMe(int n) {
-		throw  new NotImplementedException();
+	public static String speakToMe(int n){
+		
+	}	
+	
+	public static String numberToLetter(int n) {
+		
+		
 	}
 
 	/*
-	 * este metodo devuelve cierto si el año de la fecha es bisiesto fecha
+	 * este metodo devuelve cierto si el a�o de la fecha es bisiesto fecha
 	 * dd-MM-yyyy
 	 */
 	public static boolean isLeapYear(String fecha) {
-		throw  new NotImplementedException();
+		
+		
 	}
 
 	/*
-	 * este metodo devuelve cierto si la fecha es válida
+	 * este metodo devuelve cierto si la fecha es v�lida
 	 */
 	public static boolean isValidDate(String date) {
-		throw  new NotImplementedException();
+		
+					
 	}
+
 }
+
